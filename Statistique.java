2ae3dc14b8434953aec1a0e4a7048204f3407f12
Ben Jillani Mohammed <tentaculusoctopus@gmail.com>
2025-05-23 10:49:04 +0200

    Create Statistique.java
    Pour l'exo2 Mock

---
+++ Statistique.java
@@ -0,0 +1,11 @@
+public class Statistique {
+
+  Calculatrice calculatrice = new Calculatrice();
+  
+  public float moyenne(float x, float y){
+    return calculatrice.div(
+            calculatrice.add(x, y),
+            2);
+  }
+  
+}
